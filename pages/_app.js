190a37e8e4905a113b0d4e92ac376ee74fc71e61
Louis van der Weck <Louis.vanderweckgmail.com>
2023-04-12 12:08:28 +0200

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import GlobalStyle from "../styles";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
-/* import useSatet from "react";  */
-// import data from "../data/products";
-// import { products } from "../data/products";
 
 export default function App({ Component, pageProps }) {
-  // const [productid setProductid] = useState([])
-  //  function handleAppendWarenkorb (
   return (
     <>
       <GlobalStyle />
